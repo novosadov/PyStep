4a9f80ac9df11a1a6cb89db247ecb8f63e34309d
Pavel Novosadov <berk@mail.ru>
2017-04-13 18:59:02 +0300

    Update windowInCenter.py

--- PyQt/windowInCenter.py
+++ PyQt/windowInCenter.py
@@ -11,15 +11,29 @@ class Example(QWidget):
         
     def initUI(self):               
         self.resize(250, 150)
-        self.center()
+        self.center()#Код, который будет центрировать окно,
+                     #размещён в специальном методе center()
         self.setWindowTitle('Center')    
         self.show()
         
     def center(self):
-        qr = self.frameGeometry()
+        qr = self.frameGeometry()# Мы получаем прямоугольник,
+                                 # точно определяющий форму главного окна.
+                                 
         cp = QDesktopWidget().availableGeometry().center()
+        ''' Мы выясняем разрешение экрана нашего монитора.
+        Из этого разрешения, мы получаем центральную точку.'''
+                             
         qr.moveCenter(cp)
+        ''' Наш прямоугольник уже имеет высоту и ширину.
+        Теперь мы устанавливаем центр прямоугольника в центр экрана.
+        Размер прямоугольника не изменяется.'''
+
+        
         self.move(qr.topLeft())
+        '''Мы перемещаем верхнюю левую точку окна приложения
+           в верхнюю левую точку прямоугольника qr,
+           таким образом центрируя окно на нашем экране.'''
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
